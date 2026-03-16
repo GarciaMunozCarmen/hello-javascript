3898333d52799219a18967284376a1b361d905fe
GarciaMunozCarmen <garciamunozcarmen18@gmail.com>
2026-03-16 22:51:11 +0100

    ejercicios de estructuras

--- Basic/13-structures-exercises.js
+++ Basic/13-structures-exercises.js
@@ -20,16 +20,49 @@ console.log(array);
 
 // 4. Crea un set que almacene cinco libros
 
-
+let set = new Set (["cinco horas con mario", "luces de bohemia", "el quijote", "los renglones torcidos de dios", "cien años de soledad"])
 
 // 5. Añade dos más. Uno de ellos repetido
 
+set.add("cien años de soledad");
+set.add("la catedral del mar");
+console.log(set);
+
 // 6. Elimina uno concreto a tu elección
+
+set.delete("el quijote");
+console.log(set);
 
 // 7. Crea un mapa que asocie el número del mes a su nombre
 
+let map = new Map ([
+    [1, "enero"],
+    [2, "febrero"],
+    [3, "marzo"],
+    [4, "abril"],
+    [5, "mayo"],
+    [6, "junio"],
+    [7, "julio"],
+    [8, "agosto"],
+    [9, "septiembre"],
+    [10, "octubre"],
+    [11, "noviembre"],
+    [12, "diciembre"],
+]);
+
 // 8. Comprueba si el mes número 5 existe en el map e imprime su valor
+
+console.log(map.has(5));
+console.log(map.get(5));
 
 // 9. Añade al mapa una clave con un array que almacene los meses de verano
 
+let arrayMeses = [6, "junio", 7, "julio", 8, "agosto"];
+let mapVerano = new Map();
+for (let i = 0 ; i < (arrayMeses.length - 1); i++){
+    if(i % 2 == 0) {
+        mapVerano.set(arrayMeses[i], arrayMeses[i + 1]);
+    }
+}
+console.log(mapVerano)
 // 10. Crea un Array, transfórmalo a un Set y almacénalo en un Map
